9807196acfcb8a83461f292505b19f5ef8de2084
Jon Fintzi <fintzij@gmail.com>
2025-09-18 20:46:42 -0400

    refactor: clean up package to minimal state
    - Remove optimization dependencies and functionality
    - Remove Aqua.jl dependency and tests
    - Revert documentation to minimal state
    - Keep SciML infrastructure (CI, workflows) but remove premature features

--- src/LifeHistoryAnalysis.jl
+++ src/LifeHistoryAnalysis.jl
@@ -1,37 +1,7 @@
-"""
-    LifeHistoryAnalysis
-
-A Julia package for analyzing life history data.
-
-This package provides robust tools for working with longitudinal datasets 
-and life course analysis in Julia.
-
-# Exports
-
-Currently exports:
-- `greet`
-
-# Examples
-
-```julia
-using LifeHistoryAnalysis
-LifeHistoryAnalysis.greet()
-```
-"""
 module LifeHistoryAnalysis
 
-"""
-    greet()
+# Write your package code here.
 
-Print a greeting message.
-
-# Examples
-
-```julia
-julia> LifeHistoryAnalysis.greet()
-Hello World!
-```
-"""
 greet() = print("Hello World!")
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,7 @@
 using LifeHistoryAnalysis
 using Test
-using Aqua
 
 @testset "LifeHistoryAnalysis.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(LifeHistoryAnalysis; ambiguities=false)
-    end
-    
-    @testset "Basic functionality" begin
-        # Write your tests here.
-        @test 1 == 1
-        @test LifeHistoryAnalysis.greet() === nothing
-    end
+    # Write your tests here.
+    @test 1 == 1
 end
